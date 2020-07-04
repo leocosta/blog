da809dd86d4766fc6044e2a58241b158453a98ae
leonardo <leonardo@developer.local>
2020-07-04 12:45:37 -0300

    Add RedisPubSub samples

---
+++ samples/RedisPubSub/Publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Publisher");
+            Console.WriteLine();
+
+            var subscriber = CreateSubscriber();
+
+            do
+            {
+                Console.Write("Type a message: ");
+
+                var value = Console.ReadLine();
+
+                if (value.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                subscriber.Publish("MyFirstTopic", value);
+
+            } while (true);
+        }
+
+        private static ISubscriber CreateSubscriber()
+        {
+            var redis = ConnectionMultiplexer.Connect(new ConfigurationOptions
+            {
+                EndPoints = {
+                    { "localhost", 6379 }
+                },
+            });
+
+            var subscriber = redis.GetSubscriber();
+
+            return subscriber;
+        }
+    }
+}
---
+++ samples/RedisPubSub/Subscriber/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Caching.Memory;
+using StackExchange.Redis;
+
+namespace Subscriber
+{
+    class Program
+    {
+        private const string CacheKey = "Messages";
+
+        private static readonly IMemoryCache cache = new MemoryCache(new MemoryCacheOptions());
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Subscribers");
+            Console.WriteLine();
+            
+            CreateSubscriber()
+                .Subscribe("MyFirstTopic", (channel, message) =>
+                {
+                    SaveMessage(message);
+                    ClearConsole();
+                    ShowMessages();
+                });
+
+            Console.ReadKey();
+        }
+
+        private static void SaveMessage(RedisValue message)
+        {
+            var messages = cache.GetOrCreate(CacheKey, i => new Dictionary<DateTime, string>());
+
+            messages.Add(DateTime.Now, message);
+
+            cache.Set(CacheKey, messages);
+        }
+
+        private static void ClearConsole()
+        {
+            Console.Clear();
+        }
+
+        private static void ShowMessages()
+        {
+            var messages = cache.Get<IDictionary<DateTime, string>>(CacheKey);
+            foreach (var item in messages)
+            {
+                Console.WriteLine($"{ item.Key }: { item.Value }");
+            }
+        }
+
+        private static ISubscriber CreateSubscriber()
+        {
+            var redis = ConnectionMultiplexer.Connect(new ConfigurationOptions
+            {
+                EndPoints = {
+                     { "localhost", 6379 }
+                },
+            });
+
+            var subscriber = redis.GetSubscriber();
+
+            return subscriber;
+        }
+    }
+}
